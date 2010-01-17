19b034b71872213ed6639e44a9c8f467cf1981dd
Martin v. L?wis <martin@v.loewis.de>
2010-01-17 23:35:25 +0100

    Make sure sdist succeeds on Python 2.5.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 setup(name='pep381client',
       version='1.0',
       description='Mirroring tool that implements the client (mirror) side of PEP 381',
-      author=u'Martin v. Löwis',
+      author=u'Martin v. Löwis'.encode("utf-8"),
       author_email='martin@v.loewis.de',
       url='http://bitbucket.org/loewis/pep381client/',
       packages=['pep381client'],
