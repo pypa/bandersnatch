516e59d600816ca2af455c179405370bfef1ba4c
Horst Gutmann <zerok@zerokspot.com>
2010-10-31 13:11:38 +0100

    Added option parsing via optparse to pep381run

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from pep381client import version
+
+setup(name='pep381client',
+      version=version,
+      description='Mirroring tool that implements the client (mirror) side of PEP 381',
+      long_description=open('README').read(),
+      author='Martin v. Loewis',
+      author_email='martin@v.loewis.de',
+      license = 'Academic Free License, version 3',
+      url='http://bitbucket.org/loewis/pep381client/',
+      packages=['pep381client'],
+      scripts=['pep381run','processlogs']
+     )
