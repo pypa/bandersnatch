3c2a096796f12d628ff3c110087283ff8aeef0e3
Christian Theune <ct@gocept.com>
2013-03-20 11:38:45 -0700

    Switch to a more reliable serial-based changelog fetching.
    Download simple index pages.

--- src/pep381client/package.py
+++ src/pep381client/package.py
@@ -14,10 +14,16 @@ class Package(object):
         self.name = name
         self.mirror = mirror
 
+
     @property
     def package_directories(self):
         return glob.glob(os.path.join(
             self.mirror.webdir, 'packages/*/{}/{}'.format(self.name[1], self.name)))
+
+    @property
+    def package_files(self):
+        return glob.glob(os.path.join(
+            self.mirror.webdir, 'packages/*/{}/{}/*'.format(self.name[1], self.name)))
 
     @property
     def simple_directory(self):
@@ -47,44 +53,60 @@ class Package(object):
             release_files.extend(self.mirror.master.release_urls(
                 self.name, release))
 
-        # Ensure we have all release files
+        self.purge_files(release_files)
+
         for release_file in release_files:
             self.download_file(release_file)
 
-        # XXX
-        # Ensure we don't keep deleted files
-        # NotImplemented()
-
-
     def sync_simple_page(self):
-        # XXX raise NotImplemented()
-        return
+        logger.info('Syncing index page for {}'.format(self.name))
+        r = requests.get(self.mirror.master.url+'/simple/'+self.name)
 
-    def download_file(self, info):
-        url = info['url']
+        if not os.path.exists(self.simple_directory):
+            os.makedirs(self.simple_directory)
+
+        simple_page = os.path.join(self.simple_directory, 'index.html')
+        with open(simple_page, 'wb') as f:
+            f.write(r.content)
+        # XXX add serversignature, check before writing out
+
+    def _file_url_to_local_path(self, url)
         path = url.replace(self.mirror.master.url, '')
-
         if not path.startswith('/packages'):
             raise RuntimeError('Got invalid download URL: {}'.format(url))
-        path = path[1:]  # Strip off leading '/'
+        path = path[1:]
+        return os.path.join(self.mirror.webdir, path)
 
-        local_path = os.path.join(self.mirror.webdir, path)
-        if os.path.exists(local_path):
-            existing_hash = utils.hash(local_path)
+    def purge_files(self, release_files):
+        master_files = [self._file_url_to_local_path(f['url'])
+                        for f in release_files]
+        existing_files = list(self.package_files)
+
+    def download_file(self, info):
+        path = self._file_url_to_local_path(info['url'])
+
+        # Avoid downloading again if we have the file and it matches the hash.
+        if os.path.exists(path):
+            existing_hash = utils.hash(path)
             if existing_hash == info['md5_digest']:
                 return
 
         logger.info('Downloading file {}'.format(url))
 
         r = requests.get(url)
-        dirname = os.path.dirname(local_path)
+        dirname = os.path.dirname(path)
         if not os.path.exists(dirname):
             os.makedirs(dirname)
-        with open(local_path, "wb") as f:
+        with open(path, "wb") as f:
             f.write(r.content)
 
-        existing_hash = utils.hash(local_path)
+        # Verify the download. Do not leave the file in place if it isn't the
+        # correct one.
+        # XXX why load again after we just touched the in-memory data a few
+        # lines above?
+        existing_hash = utils.hash(path)
         if existing_hash != info['md5_digest']:
+            os.unlink(path)
             raise ValueError('{} has hash {} instead of {}'.format(
                 url, existing_hash, info['md5_digest']))
 
