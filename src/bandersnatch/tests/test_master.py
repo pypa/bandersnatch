ccf1de6deedb81e3a2578e36c4a452c42cc7bea2
Christian Theune <ct@gocept.com>
2013-03-21 14:40:52 -0700

    More tests.

--- src/bandersnatch/tests/test_master.py
+++ src/bandersnatch/tests/test_master.py
@@ -1,6 +1,8 @@
 from bandersnatch.master import Master
-import pytest
 import mock
+import pytest
+import xmlrpclib
+
 
 @pytest.fixture
 def master():
@@ -8,18 +10,26 @@ def master():
     master.rpc = mock.Mock()
     return master
 
+
+def test_rpc_factory():
+    master = Master('http://pypi.example.com')
+    assert isinstance(master.rpc(), xmlrpclib.ServerProxy)
+
+
 def test_rpc_url(master):
     assert master.xmlrpc_url == 'http://pypi.example.com/pypi/'
 
+
 def test_list_packages(master):
     master.list_packages()
-    assert master.rpc().list_packages.called
+
 
 def test_changed_packages_no_changes(master):
     master.rpc().changelog_since_serial.return_value = []
     changes, last_serial = master.changed_packages(4)
     assert list(changes) == []
     assert last_serial == 4
+
 
 def test_changed_packages_with_changes(master):
     master.rpc().changelog_since_serial.return_value = [
@@ -28,3 +38,15 @@ def test_changed_packages_with_changes(master):
     changes, last_serial = master.changed_packages(4)
     assert list(changes) == ['foobar', 'baz']
     assert last_serial == 18
+
+
+def test_package_releases(master):
+    master.package_releases('foobar')
+
+
+def test_release_urls(master):
+    master.release_urls('foobar', '0.1')
+
+
+def test_get_current_serial(master):
+    master.get_current_serial()
