688e1c37dbef81035911866deb70f1deb9929444
Christian Theune <ct@gocept.com>
2013-03-25 21:52:15 +0100

    Make rewriting files atomic to avoid short reads from webservers during updates.

--- src/bandersnatch/utils.py
+++ src/bandersnatch/utils.py
@@ -1,5 +1,8 @@
+import contextlib
 import hashlib
 import os
+import os.path
+import tempfile
 
 
 def hash(path, function='md5'):
@@ -25,3 +28,13 @@ def find(root, dirs=True):
             results.append(os.path.join(dirpath, name))
     results.sort()
     return '\n'.join(result.replace(root, '', 1) for result in results)
+
+
+@contextlib.contextmanager
+def rewrite(filename):
+    """Rewrite an existing file atomically to avoid programs running in
+    parallel to have race conditions while reading."""
+    _, filename_tmp = tempfile.mkstemp(dir=os.path.dirname(filename))
+    with open(filename_tmp, 'w') as f:
+        yield f
+    os.rename(filename_tmp, filename)
