d632e822e133ed77c402b5720a9c1e6f83d97f55
Christian Theune <ct@gocept.com>
2013-04-09 18:57:29 +0200

    Try to integrate requirements.txt updating with zest.releaser.

---
+++ src/bandersnatch/release.py
@@ -0,0 +1,19 @@
+from zest.releaser.utils import system
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+
+def update_requirements(data):
+    os.chdir(data['workingdir'])
+    logging.info('Running buildout to update requirements.txt.')
+    system('bin/buildout')
+    logging.info('Committing requirements.txt.')
+    system('hg commit -v -m "Update requirements.txt"')
+
+
+def update_stable_tag(data):
+    os.chdir(data['workingdir'])
+    logging.info('Updating stable tag.')
+    system('hg tag -r %s stable' % data['version'])
