27e673da5d4e7dea8a20fed95430753fa9fcac70
Monty Taylor <mordred@inaugust.com>
2014-07-03 07:24:15 -0700

    Add ability to configure logging with config file
    Python logging has really flexible logging config ability via logging
    config files. For folks running production bandersnatch mirrors, the
    ability to do logging in a more controlled manner is quite helpful.

--- src/bandersnatch/tests/test_main.py
+++ src/bandersnatch/tests/test_main.py
@@ -1,5 +1,6 @@
 from bandersnatch.main import main
 import bandersnatch.mirror
+import logging.config
 import mock
 import os.path
 import pytest
@@ -50,11 +51,29 @@ def test_main_reads_config_values(mirror_mock):
     assert mirror_mock().synchronize.called
 
 
+def test_main_reads_log_config_value(mirror_mock, logging_mock, tmpconfig):
+    config = str(tmpconfig/'with-logging.conf')
+    sys.argv = ['bandersnatch', '-c', config, 'mirror']
+    assert os.path.exists(config)
+    assert isinstance(bandersnatch.mirror.Mirror, mock.Mock)
+    assert isinstance(logging.config.fileConfig, mock.Mock)
+    main()
+    (log_config, kwargs) = logging_mock.call_args_list[0]
+    assert log_config == (str(tmpconfig/'bandersnatch-log.conf'),)
+    assert mirror_mock().synchronize.called
+
+
 @pytest.fixture
 def tmpconfig(tmpdir):
     default = os.path.dirname(bandersnatch.__file__) + '/default.conf'
+    config = open(default).read()
+    config = config.replace('/srv/pypi', str(tmpdir/'pypi'))
     with open(str(tmpdir/'bandersnatch.conf'), 'w') as f:
-        config = open(default).read()
-        config = config.replace('/srv/pypi', str(tmpdir/'pypi'))
+        f.write(config)
+    config = config.replace('; log-config', 'log-config')
+    config = config.replace(
+        '/etc/bandersnatch-log.conf',
+        str(tmpdir/'bandersnatch-log.conf'))
+    with open(str(tmpdir/'with-logging.conf'), 'w') as f:
         f.write(config)
     return tmpdir
