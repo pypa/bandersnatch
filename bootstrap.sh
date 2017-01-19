6e48e5e6bd87acb270b751b2364edb85ae808150
Christian Theune <ct@flyingcircus.io>
2017-01-19 11:59:39 +0100

    make bootstrap a bit more robust

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,8 @@
 
 if [ ! -e bin/python2.7 ]; then
     virtualenv .
-    bin/pip install zc.buildout
-    bin/pip install virtualenv
 fi
+bin/pip install --upgrade zc.buildout==2.5.3
+bin/pip install --upgrade setuptools==33.1.1
+bin/pip install --upgrade virtualenv==15.1.0
 bin/buildout
