74fc9921579c0c47f2f70e46a0a3c07bdfe6b180
Christian Theune <ct@flyingcircus.io>
2017-04-05 14:13:30 +0200

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bandersnatch',
-      version='2.0.0',
+      version='2.0.1.dev0',
       description='Mirroring tool that implements the client (mirror) side of PEP 381',
       long_description='\n\n'.join(
           [open('README').read(), open('CHANGES.txt').read()]),
