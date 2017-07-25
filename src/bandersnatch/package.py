fe4c0e52ae51952e2e1a9c7dadaa9da04a3f65dc
Cooper Lees <me@cooperlees.com>
2017-07-24 20:55:39 -0700

    Sort by releases by filename for package index.html - Sorting by the full URL results in a non friendly basically unsorted simple HTML for a human -- One day I might ask and find out what the hashing is actually for - It is nice from time to time to go grab source or look at what the latest is by scrolling to the bottom and not having to use ctrl+f etc. Testing: - Unit tests running: 90 passed in 3.14 seconds

--- src/bandersnatch/package.py
+++ src/bandersnatch/package.py
@@ -134,7 +134,10 @@ class Package(object):
         release_files = []
         for release in self.releases.values():
             release_files.extend(release)
-        release_files.sort(key=lambda x: x["url"])
+        # Lets sort based on the filename rather than the whole URL
+        release_files.sort(
+            key=lambda x: os.path.basename(urlparse(x["url"]).path)
+        )
 
         simple_page_content += '\n'.join([
             '<a href="{0}#md5={1}">{2}</a><br/>'.format(
