d69d3a9e0060167b219149b7567949abb451ca3a
Cooper Lees <me@cooperlees.com>
2017-07-25 09:27:36 -0700

    Tweak rewrite util - Make it more friendly to distrubuted POSIX systems - e.g. GlusterFS - This stops a rehash sometimes causing the 'rename' to not function correctly - Also makes temp files hidden and not show up in the directory listing in the mirror -- Would reccommend having a cleaner process - Might eventuall open source a verifyer I plan working on - Unit tests still all pass

---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='bandersnatch',
+      version='2.0.1.dev0',
+      description='Mirroring tool that implements the client (mirror) side of PEP 381',
+      long_description='\n\n'.join(
+          [open('README').read(), open('CHANGES.txt').read()]),
+      author='Christian Theune',
+      author_email='ct@flyingcircus.io',
+      license='Academic Free License, version 3',
+      url='http://bitbucket.org/pypa/bandersnatch/',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      install_requires=[
+          'setuptools',
+          'mock',
+          'packaging>=16.2',
+          'pytest',
+          'pytest-catchlog',
+          'pytest-codecheckers',
+          'pytest-cov',
+          'pytest-timeout',
+          'pytest-cache',
+          'requests',
+          'xmlrpc2'],
+      entry_points="""
+            [console_scripts]
+                bandersnatch = bandersnatch.main:main
+            [zc.buildout]
+                requirements = bandersnatch.buildout:Requirements
+
+            [zest.releaser.prereleaser.after]
+                update_requirements = bandersnatch.release:update_requirements
+            [zest.releaser.releaser.after]
+                update_stable_tag = bandersnatch.release:update_stable_tag
+            [zest.releaser.postreleaser.after]
+                update_requirements = bandersnatch.release:update_requirements
+      """,
+      classifiers=[
+          'Programming Language :: Python :: 3.5'])
