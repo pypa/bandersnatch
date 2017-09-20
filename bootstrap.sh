ddd0bc9a9b843bd603870e5a468d532ba9d32d87
Cooper Lees <me@cooperlees.com>
2017-09-20 00:36:57 +0000

    default.conf edited to include nginx.conf example rewrite rules

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -e bin/python3 ]; then
+    virtualenv --python=python3.5 .
+fi
+if [ ! -e bin/buildout ]; then
+    bin/pip install zc.buildout
+    bin/pip install virtualenv
+fi
+bin/pip install --upgrade zc.buildout==2.5.3
+bin/pip install --upgrade setuptools==33.1.1
+bin/pip install --upgrade virtualenv==15.1.0
+bin/buildout
---
+++ src/bandersnatch/utils.py
@@ -0,0 +1,74 @@
+import contextlib
+import hashlib
+import os
+import os.path
+import pkg_resources
+import sys
+import tempfile
+
+
+def user_agent():
+    template = 'bandersnatch/{version} ({python}, {system})'
+    system = os.uname()
+    system = ' '.join([system[0], system[4]])
+    version = pkg_resources.require("bandersnatch")[0].version
+    # Support Python 2 + 3 - No sys.subversion in Py3
+    try:
+        python = sys.subversion[0]
+    except AttributeError:
+        python = sys.implementation.name
+    python += ' {0}.{1}.{2}-{3}{4}'.format(*sys.version_info)
+    return template.format(**locals())
+
+USER_AGENT = user_agent()
+
+
+def hash(path, function='md5'):
+    h = getattr(hashlib, function)()
+    with open(path, 'rb') as f:
+        while True:
+            chunk = f.read(128 * 1024)
+            if not chunk:
+                break
+            h.update(chunk)
+    return h.hexdigest()
+
+
+def find(root, dirs=True):
+    """A test helper simulating 'find'.
+
+    Iterates over directories and filenames, given as relative paths to the
+    root.
+
+    """
+    results = []
+    for dirpath, dirnames, filenames in os.walk(root):
+        names = filenames
+        if dirs:
+            names += dirnames
+        for name in names:
+            results.append(os.path.join(dirpath, name))
+    results.sort()
+    return '\n'.join(result.replace(root, '', 1) for result in results)
+
+
+@contextlib.contextmanager
+def rewrite(filepath, mode='w', *args, **kw):
+    """Rewrite an existing file atomically to avoid programs running in
+    parallel to have race conditions while reading."""
+    base_dir = os.path.dirname(filepath)
+    filename = os.path.basename(filepath)
+    # Change naming format to be more friendly with distributed POSIX
+    # filesystems like GlusterFS that hash based on filename
+    # GlusterFS ignore '.' at the start of filenames and this avoid rehashing
+    with tempfile.NamedTemporaryFile(mode=mode, prefix='.{}.'.format(filename),
+                                     delete=False, dir=base_dir, **kw) as f:
+        filepath_tmp = f.name
+        yield f
+
+    if not os.path.exists(filepath_tmp):
+        # Allow our clients to remove the file in case it doesn't want it to be
+        # put in place actually but also doesn't want to error out.
+        return
+    os.chmod(filepath_tmp, 0o100644)
+    os.rename(filepath_tmp, filepath)
