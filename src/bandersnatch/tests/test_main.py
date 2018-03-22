4718db28ac4b5e5c27e5874409fe2dec58b15eb8
Ernest W. Durbin III <ewdurbin@gmail.com>
2018-03-22 13:00:22 -0400

    actually allow root_uri to be configured

--- src/bandersnatch/tests/test_main.py
+++ src/bandersnatch/tests/test_main.py
@@ -46,6 +46,7 @@ def test_main_reads_config_values(mirror_mock):
             'stop_on_error': False,
             'hash_index': False,
             'workers': 3,
+            'root_uri': None,
             'json_save': False,
             'package_blacklist': ['', 'example1', 'example2']} == kwargs
     assert mirror_mock().synchronize.called
