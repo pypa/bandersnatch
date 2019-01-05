eaa247e4526686ac6d5047955bec408d9be4e534
Cooper Ry Lees <me@cooperlees.com>
2019-01-05 15:58:44 -0800

    Test + Refactor verify - I was lazy and never added tests ... So here are some - Moved utility functions to utils - Add more type hinting why at it
    Coverage now on verify matches the rest of bandersnatch

--- src/bandersnatch/tests/test_utils.py
+++ src/bandersnatch/tests/test_utils.py
@@ -1,9 +1,31 @@
 import os
 import os.path
+import re
+from pathlib import Path
+from tempfile import TemporaryDirectory, gettempdir
 
 import pytest
 
-from bandersnatch.utils import hash, rewrite, user_agent
+from bandersnatch.utils import (  # isort:skip
+    convert_url_to_path,
+    hash,
+    recursive_find_files,
+    rewrite,
+    unlink_parent_dir,
+    user_agent,
+)
+
+
+def test_convert_url_to_path():
+    assert (
+        "packages/8f/1a/1aa000db9c5a799b676227e845d2b64fe725328e05e3d3b30036f"
+        + "50eb316/peerme-1.0.0-py36-none-any.whl"
+        == convert_url_to_path(
+            "https://files.pythonhosted.org/packages/8f/1a/1aa000db9c5a799b67"
+            + "6227e845d2b64fe725328e05e3d3b30036f50eb316/"
+            + "peerme-1.0.0-py36-none-any.whl"
+        )
+    )
 
 
 def test_hash():
@@ -15,6 +37,22 @@ def test_hash():
     assert hash(sample) == (
         "95c07c174663ebff531eed59b326ebb3fa95f418f680349fc33b07dfbcf29f18"
     )
+
+
+def test_find_files():
+    with TemporaryDirectory() as td:
+        td_path = Path(td)
+        td_sub_path = td_path / "aDir"
+        td_sub_path.mkdir()
+
+        expected_found_files = {td_path / "file1", td_sub_path / "file2"}
+        for afile in expected_found_files:
+            with afile.open("w") as afp:
+                afp.write("PyPA ftw!")
+
+        found_files = set()
+        recursive_find_files(found_files, td_path)
+        assert found_files == expected_found_files
 
 
 def test_rewrite(tmpdir, monkeypatch):
@@ -41,11 +79,20 @@ def test_rewrite_fails(tmpdir, monkeypatch):
 
 def test_rewrite_nonexisting_file(tmpdir, monkeypatch):
     monkeypatch.chdir(tmpdir)
-    with rewrite("sample") as f:
+    with rewrite("sample", "w") as f:
         f.write("csdf")
-    assert open("sample").read() == "csdf"
+    with open("sample", "r") as f:
+        assert f.read() == "csdf"
+
+
+def test_unlink_parent_dir():
+    adir = Path(gettempdir()) / f"tb.{os.getpid()}"
+    adir.mkdir()
+    afile = adir / "file1"
+    afile.touch()
+    unlink_parent_dir(afile)
+    assert not adir.exists()
 
 
 def test_user_agent():
-    ua = user_agent()
-    assert "bandersnatch/" in ua
+    assert re.match(r"bandersnatch/[0-9]\.[0-9]\.[0-9] \(.*\)", user_agent())
