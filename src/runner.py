23d57ac730302afe0d512222f856dcfd7e9bbe52
GreatBahram <aghaee.bahram@gmail.com>
2020-01-07 19:42:17 +0330

    bugfix in runner.py:
    * Change type of interval argument to int
    * Remove hardcoded bandersnatch path; instead run it by using the bandersnatch.main module

--- src/runner.py
+++ src/runner.py
@@ -4,27 +4,36 @@
 
 import argparse
 import sys
-from subprocess import run
+from subprocess import run, CalledProcessError
 from time import sleep, time
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
+    parser = argparse.ArgumentParser()
     parser.add_argument(
         "-c",
         "--config",
         default="/conf/bandersnatch.conf",
         help="Configuration location",
     )
-    parser.add_argument("interval", help="Time in seconds between runs")
+    parser.add_argument("interval", help="Time in seconds between runs", type=int)
     args = parser.parse_args()
 
     print(f"Running bandersnatch every {args.interval}s", file=sys.stderr)
     while True:
         start_time = time()
-        run(["/usr/bin/bandersnatch", "--config", args.conf, "mirror"])
+        try:
+            cmd = [
+                sys.executable,
+                "-m",
+                "bandersnatch.main",
+                "--config",
+                args.config,
+                "mirror",
+            ]
+            run(cmd, check=True)
+        except CalledProcessError:
+            sys.exit(1)
         run_time = time() - start_time
         if run_time < args.interval:
             sleep_time = args.interval - run_time
