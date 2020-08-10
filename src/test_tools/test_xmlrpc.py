dd10f3f199ee6ff158c47d2c7faab96b6bd9ec46
Cooper Lees <me@cooperlees.com>
2020-08-09 17:29:12 -0700

    Increase CI aiohttp timeout to 60 seconds (#648)
    * Double CI Timeout
    - Having lots of GitHub actions errors with XMLRPC
    - Going to double aiohttp timeout on the xmlrpc calls to see if this stabilizes it
    - Refer to #647 for the testing I did
    
    Fixes #647
    
    * Also double swift ci config
    
    * Try 60 seconds ...
    
    * Have mypy ignore asyncio.run use in the new xmlrpc test tool - Mypy is set to 3.6

---
+++ src/test_tools/test_xmlrpc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Quick tool to test xmlrpc queries from bandersnatch
+- Note this is >= 3.7 only due to asyncio.run
+"""
+
+import asyncio
+
+from bandersnatch.master import Master
+
+
+async def main() -> int:
+    async with Master("https://pypi.org") as master:
+        all_packages = await master.all_packages()
+    print(f"PyPI returned {len(all_packages)} PyPI packages via xmlrpc")
+    return 0
+
+
+if __name__ == "__main__":
+    exit(asyncio.run(main()))  # type: ignore
