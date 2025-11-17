2bfba43b524c8c006492a47c63f0f34c7b02622c
Cooper Ry Lees <me@cooperlees.com>
2025-11-16 19:21:31 -0600

    Move main to 7.0.0.dev0 version as some big changes coming

--- src/bandersnatch/__init__.py
+++ src/bandersnatch/__init__.py
@@ -19,10 +19,10 @@ class _VersionInfo(NamedTuple):
 
 
 __version_info__ = _VersionInfo(
-    major=6,
-    minor=8,
+    major=7,
+    minor=0,
     micro=0,
-    releaselevel="",
+    releaselevel="dev0",
     serial=0,  # Not currently in use with Bandersnatch versioning
 )
 __version__ = __version_info__.version_str
