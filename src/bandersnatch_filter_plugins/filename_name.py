2491f7ff3139b3518494d151e1779fe366fcb91b
Cooper Lees <me@cooperlees.com>
2026-04-02 16:03:28 -0500

    Fix indentation for Python version list

--- src/bandersnatch_filter_plugins/filename_name.py
+++ src/bandersnatch_filter_plugins/filename_name.py
@@ -37,7 +37,7 @@ class ExcludePlatformFilter(FilterReleaseFilePlugin):
         "py3.11",
         "py3.12",
         "py3.13",
-        "py3.14"
+        "py3.14",
     ]
 
     _windowsPlatformTypes = [".win32", "-win32", "win_amd64", "win-amd64"]
