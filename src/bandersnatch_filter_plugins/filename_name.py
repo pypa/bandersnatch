214685bedc150492321532d96490437e236011b1
Cooper Lees <me@cooperlees.com>
2026-04-03 08:52:19 -0500

    Refactor _windowsPlatformTypes for black
    Reformatted the _windowsPlatformTypes list for better readability.

--- src/bandersnatch_filter_plugins/filename_name.py
+++ src/bandersnatch_filter_plugins/filename_name.py
@@ -39,7 +39,14 @@ class ExcludePlatformFilter(FilterReleaseFilePlugin):
         "py3.13",
     ]
 
-    _windowsPlatformTypes = [".win32", "-win32", "win_amd64", "win-amd64", "win_arm64", "win-arm64"]
+    _windowsPlatformTypes = [
+        ".win32",
+        "-win32",
+        "win_amd64",
+        "win-amd64",
+        "win_arm64",
+        "win-arm64",
+    ]
 
     _linuxPlatformTypes = [
         "linux-i686",  # PEP 425
